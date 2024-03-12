afa7bc4929b3f8c12752678bb83522a5928395f7
Roberto Gomez Mendoza <desarrollador10@dalton.com.mx>
2024-03-12 16:38:35 -0600

    Version Inicial

---
+++ aaadevhelloworld-widget.js
@@ -0,0 +1,33 @@
+//Definicion de Librerias
+['https://cnd.jsdelivr.net/npm/axios/dist/axios.min.js'].forEach(u =>
+{
+    var script = document.createElement('script');
+    script.src = u;
+    document.head.appendChild(script);
+});
+
+class TestWebComponentWidget extends HTMLElement{
+    static _WIDGETNAME = 'aaadevhelloworld-widget';
+    static _WIDGETURL = 'htpps://{{fqdn}}';
+}
+//función que se ejecuta una vez que el custom element es intanciado en la inteface del worksapace
+function connectecCallback(){
+  this.classList.add('neo-widget');
+  this.classList.add(`wigget--${TestWebComponentWidget._WIDGETNAME}`)
+  this.style.overflowX = 'hidden'
+
+
+  fetch(`${TestWebComponentWidget._WIDGETURL}/${TestWebComponentWidget._WIDGETNAME}.html`)
+  .then(data => data.text)
+  .then(html => {
+     this.innerHTML += `<link rel="stylesheet" href= "${TestWebComponentWidget._WIDGETURL}/${TestWebComponentWidget._WIDGETNAME}.css" `
+     this.innerHTML += html;
+     this.initWidgetCode();
+  })
+};
+
+function initWidgetCode(){
+    console.log('Init widget code');
+}
+
+var widget = customElements.define(TestWebComponentWidget._WIDGETNAME,TestWebComponentWidget);
