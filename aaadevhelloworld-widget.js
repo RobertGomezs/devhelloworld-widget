4370366d3df9682dd63530b4da339d2a8192775d
Roberto Gomez Mendoza <desarrollador10@dalton.com.mx>
2024-03-13 16:38:01 -0600

    ajustes

--- aaadevhelloworld-widget.js
+++ aaadevhelloworld-widget.js
@@ -9,25 +9,28 @@
 class TestWebComponentWidget extends HTMLElement{
     static _WIDGETNAME = 'aaadevhelloworld-widget';
     static _WIDGETURL = 'https://blue-flower-040073910.5.azurestaticapps.net/';
+    
+
+    //función que se ejecuta una vez que el custom element es intanciado en la inteface del worksapace
+    connectedCallback(){
+        this.classList.add('neo-widget');
+        this.classList.add(`widget--${TestWebComponentWidget._WIDGETNAME}`);
+        this.style.overflowX = 'hidden'
+
+        
+        fetch(`${TestWebComponentWidget._WIDGETURL}/${TestWebComponentWidget._WIDGETNAME}.html`)
+        .then(data => data.text())
+        .then(html => {
+            this.innerHTML += `<link rel="stylesheet" href= "${TestWebComponentWidget._WIDGETURL}/${TestWebComponentWidget._WIDGETNAME}.css" `
+            this.innerHTML += html;
+            this.initWidgetCode();
+        })
+    }
+
+    initWidgetCode(){
+        console.log('Init widget code');
+    }
 }
-//función que se ejecuta una vez que el custom element es intanciado en la inteface del worksapace
-function connectecCallback(){
-  this.classList.add('neo-widget');
-  this.classList.add(`wigget--${TestWebComponentWidget._WIDGETNAME}`)
-  this.style.overflowX = 'hidden'
 
 
-  fetch(`${TestWebComponentWidget._WIDGETURL}/${TestWebComponentWidget._WIDGETNAME}.html`)
-  .then(data => data.text)
-  .then(html => {
-     this.innerHTML += `<link rel="stylesheet" href= "${TestWebComponentWidget._WIDGETURL}/${TestWebComponentWidget._WIDGETNAME}.css" `
-     this.innerHTML += html;
-     this.initWidgetCode();
-  })
-};
-
-function initWidgetCode(){
-    console.log('Init widget code');
-}
-
-var widget = customElements.define(TestWebComponentWidget._WIDGETNAME,TestWebComponentWidget);
+window.customElements.define(TestWebComponentWidget._WIDGETNAME,TestWebComponentWidget);
